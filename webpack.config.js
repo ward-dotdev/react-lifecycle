c60d181bd3d7ef4b82bf2005bac939b986e5108f
Ward Oosterlijnck <ward@dotdev.com.au>
2017-08-16 21:52:53 +1000

    :tada: Basic webpack config for React app

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const {resolve} = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const NODE_ENV = process.env.NODE_ENV || 'development'
+const TITLE = require('./package.json').name
+  .replace(/-/g, ' ')
+  .replace(/\w\S*/g, str => str.charAt(0).toUpperCase() + str.substr(1).toLowerCase())
+
+module.exports = {
+  context: resolve(__dirname, 'src'),
+  entry: [
+    'react-hot-loader/patch',
+    './index'
+  ],
+  output: {
+    path: resolve(__dirname, 'build'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(NODE_ENV),
+      'process.env.TITLE': JSON.stringify(TITLE)
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './index.ejs',
+      title: TITLE
+    })
+  ]
+}
